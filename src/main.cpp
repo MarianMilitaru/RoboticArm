93b20dbebe28087b94378fe3bf8f7133f1ae3cf1
MarianMilitaru <marianmary5072@gmail.com>
2025-07-09 16:57:44 +0300

    Edited port

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,27 @@
 #define dirPin 5
 
 void setup() {
-  DDRB |= (1 << stepPin); // Output
-  DDRB |= (1 << dirPin); // Output
-  PORTB &= ~(1 << stepPin); // Low
-  PORTB &= ~(1 << dirPin); // Low
+  DDRD |= (1 << stepPin); // Output
+  DDRD |= (1 << dirPin); // Output
+
+  PORTD &= ~(1 << stepPin); // Low
+  PORTD &= ~(1 << dirPin); // Low
 }
 void loop() {
-  PORTB |= (1 << dirPin); // High
+  PORTD |= (1 << dirPin); // High
   for (int i = 0; i < 800; i++) {
-    PORTB |= (1 << stepPin); // High
+    PORTD |= (1 << stepPin); // High
     delayMicroseconds(700);
-    PORTB &= ~(1 << stepPin); // Low
+    PORTD &= ~(1 << stepPin); // Low
     delayMicroseconds(700);
   }
   delay(1000);
 
-  PORTB &= ~(1 << dirPin); // Low
+  PORTD &= ~(1 << dirPin); // Low
   for (int i = 0; i < 1600; i++) {
-    PINB |= (1 << stepPin); // High
+    PIND |= (1 << stepPin); // High
     delayMicroseconds(500);
-    PINB &= ~(1 << stepPin); // Low
+    PIND &= ~(1 << stepPin); // Low
     delayMicroseconds(500);
   }
   delay(1000);
