bf7597542328392566d20b791163bc61d328e4d9
MarianMilitaru <marianmary5072@gmail.com>
2025-07-10 13:42:01 +0300

    1 CW Rev and 2 CCW Rev

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void setup() {
 }
 void loop() {
   PORTD |= (1 << dirPin); // High
-  for (int i = 0; i < 800; i++) {
+  for (int i = 0; i < 200; i++) {
     PORTD |= (1 << stepPin); // High
     delayMicroseconds(700);
     PORTD &= ~(1 << stepPin); // Low
@@ -24,11 +24,11 @@ void loop() {
   delay(1000);
 
   PORTD &= ~(1 << dirPin); // Low
-  for (int i = 0; i < 1600; i++) {
-    PIND |= (1 << stepPin); // High
-    delayMicroseconds(300);
-    PIND &= ~(1 << stepPin); // Low
-    delayMicroseconds(300);
+  for (int i = 0; i < 400; i++) {
+    PORTD |= (1 << stepPin); // High
+    delayMicroseconds(700);
+    PORTD &= ~(1 << stepPin); // Low
+    delayMicroseconds(700);
   }
   delay(1000);
 }
